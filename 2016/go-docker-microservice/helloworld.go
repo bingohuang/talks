4ec7ab71513099eac3ad97cca6aafb3d1ecd1f4b
黄庆兵 <hzhuangqingbin@corp.netease.com>
2016-11-15 09:37:29 +0800

    Update go-docker-microservice slide - V2.1

--- 2016/go-docker-microservice/helloworld.go
+++ 2016/go-docker-microservice/helloworld.go
@@ -9,7 +9,7 @@ import "fmt"
 func Reverse(s string) string {
 	r := []rune(s)
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
-		r[i], r[j] = r[j], r[i]
+		r[i], r[j] = r[j], r[i] // 先计算出所有右值，然后再依次完成赋值操作
 	}
 	return string(r)
 }
