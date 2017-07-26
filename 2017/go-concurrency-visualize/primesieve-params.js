ac1354a81a97fbbfc01cd7961edbc16bdd734679
Bingo Huang <me@bingohuang.com>
2017-07-26 17:37:31 +0800

    Update go-concurrency-visualize

--- 2017/go-concurrency-visualize/primesieve-params.js
+++ 2017/go-concurrency-visualize/primesieve-params.js
@@ -1,8 +1,18 @@
 var params = {
   camera: "perspective",
-  allCaps: true,
-  speed: 3.8,
-  angle: 15,
+  allCaps: false,
+  speed: 2.8,
+  angle: 10,
   zoom: 1.0,
+  autoRotate: 36,
   cameraPos: {x: 120, y: 60, z: 120}
 }
+
+// var params = {
+//   camera: "perspective",
+//   allCaps: true,
+//   speed: 3.8,
+//   angle: 15,
+//   zoom: 1.0,
+//   cameraPos: {x: 120, y: 60, z: 120}
+// }
