957a9e3e0ed52f34609f56f61b6a779676cf84fb
Bingo Huang <me@bingohuang.com>
2017-07-26 10:16:17 +0800

    Update go-concurrency-visualize: add parallelism

---
+++ 2017/go-concurrency-visualize/parallelism-params.js
@@ -0,0 +1,11 @@
+var params = {
+  camera: "perspective",
+  allCaps: false,
+  speed: 1.3,
+  angle: 10,
+  zoom: 2.0,
+  autoAngle: false,
+  autoRotate: true,
+  distance: 80,
+  cameraPos: {x: 110, y: 60, z: 160}
+}
